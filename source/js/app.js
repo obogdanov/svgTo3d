5349de151106b7e6b9f6684ca76a3c2be2b627b3
mathcode <mathcode@yandex.ru>
2017-08-17 17:52:38 +0300

    minor editing

--- source/js/app.js
+++ source/js/app.js
@@ -13,48 +13,46 @@ $(function () {
 		lineCoord.y1 = parseInt(el.attr('y1'), 10),
 		lineCoord.x2 = parseInt(el.attr('x2'), 10),
 		lineCoord.y2 = parseInt(el.attr('y2'), 10);
-		
 		coord.push(lineCoord);
 	})
 
-	// include into function
-	// var lineParameters = [];
-	// ang = Math.atan2((y2 - y1)/(x2 - x1));
-	// lineLength = Math.sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
-	// centX = (x2 - x1)/2 + x1;
-	// centY = (y2 - y1)/2 + y1;
-	//
-var parameters = [];
 
-$(function () {
+	var parameters = [];
+
 	$(coord).each(function () {
-		var curLinePar = {},
+		var linePar = {},
 			x1 = $(this)[0].x1,
 			x2 = $(this)[0].x2,
 			y1 = $(this)[0].y1,
 			y2 = $(this)[0].y2;
-		// console.log($(this)[0]);
-		curLinePar.ang = Math.round(Math.atan((y2 - y1)/(x2 - x1))*Math.PI/180);
-		curLinePar.lineLength = Math.sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
-		curLinePar.centX = (x2 - x1)/2 + x1;
-		curLinePar.centY = (y2 - y1)/2 + y1;
-		parameters.push(curLinePar);
+		linePar.ang = Math.round(Math.atan((y2 - y1)/(x2 - x1))/Math.PI*180);
+		linePar.lineLength = Math.sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
+		linePar.centX = (x2 - x1)/2 + x1;
+		linePar.centY = (y2 - y1)/2 + y1;
+		parameters.push(linePar);
 	})
-})
-console.log(parameters);
 
-$(coord).each(function () {
-	var transformX = (this.x2-this.x1)/2;
+// Applying CSS and creating elements
+
+$(parameters).each(function () {
+	
+	var transformX = this.centX,
+		transformZ = this.lineLength/2;
+		ang 	   = this.ang;
+
 	var element = $('<div/>', {
 		class: 'element',
 	});
+
 	$(element).css({
-		'transform':'translateX('+transformX+'px) translateZ(50px) rotate3d(0,1,0,90deg)'
+		'transform':'translateX('+transformX+'px) translateZ('+transformZ+'px) rotate3d(0,'+ang+',0,90deg)'
 	});
+
 	$('.object').append(element);
-	// console.log(this);
+	
 })
 
+// Rotating with mouse
 
 	$(function(){
 		$(document).mousemove(function(e){
